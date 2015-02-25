cedf972f1813ede4cf3f0142452cf6be1b0e0100
A-Beck <adb2jb@virginia.edu>
2015-02-25 14:41:02 -0500

    Building a graph, provide more structure

---
+++ src/Node.java
@@ -0,0 +1,46 @@
+package src;
+
+import java.util.ArrayList;
+import java.awt.Point;
+
+/**
+ * Created by Andrew on 2/25/2015.
+ */
+public class Node {
+    private String symbol;
+    private ArrayList<Node> neighbors;
+    private Point position;
+
+    public Node (String sym, Point pos) {
+        this.symbol = sym;
+        this.position = pos;
+        this.neighbors = new ArrayList<Node>();
+    }
+
+    public boolean contains(Node n) {
+        return this.neighbors.contains(n);
+    }
+
+    public void add_neighbor(Node candidate) {
+        if (!this.contains(candidate)) {
+            neighbors.add(candidate);
+        }
+        if (!candidate.contains(this)) {
+            candidate.add_neighbor(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+
+        if (!position.equals(node.position)) return false;
+        if (!symbol.equals(node.symbol)) return false;
+
+        return true;
+    }
+
+}
--- src/myRobot.java
+++ src/myRobot.java
@@ -7,28 +7,86 @@ package src;
 import world.Robot;
 import world.World;
 import java.awt.Point;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class myRobot extends Robot {
 
-    public void travelToDestination() {
-        // see if this point is available
-        Point start = this.getPosition();
-        System.out.print("Starting Location: ");
-        System.out.println(start);
-        Point next = new Point(start.x +1, start.y);
-        String thing_at_next = super.pingMap(next);
-        System.out.println(thing_at_next);
-        if (thing_at_next.equals("O")) {
-            System.out.print("Moving to valid Point: ");
-            System.out.println(super.move( next ));
-        }
-        System.out.print("Moving to invalid point (not adjacent): ");
-        System.out.println(super.move(new Point(5, 3)));
+    private int numRows;
+    private  int numCols;
+    private Point endPos;
+    private boolean is_uncertian;
+
+    public myRobot(int numCols, int numRows, Point endPos, boolean uncert_flag) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.endPos = endPos;
+        this.is_uncertian = uncert_flag;
     }
+
+    public void travelToDestination() {
+
+        if (this.is_uncertian) {
+            System.out.println("Run uncertian Pathfinder");
+        }
+
+        else {
+            System.out.println("Run the certian pathfinder");
+            this.run_a_star();
+            this.move(new Point(1,1));
+            this.move(new Point(2,0));
+
+        }
+
+
+    }
+
+    public void run_a_star() {
+        System.out.println("running A* algo to find path");
+        ArrayList<Node> graph = construct_graph();
+        System.out.println("Constructed Graph");
+    }
+
+    public ArrayList<Node> construct_graph() {
+        ArrayList<Node> graph = new ArrayList<Node>();
+        Node[][] temp = new Node[this.numCols][this.numRows];
+
+        for (int i = 0; i < this.numCols; i++) {
+            for (int j = 0; j < this.numRows; j++) {
+                Point pt = new Point(i,j);
+                String current_obj = this.pingMap(pt);
+                Node n = new Node(current_obj, pt);
+                temp[i][j] = n;
+                if (( j - 1) >= 0) {
+                    // the position to the top
+                    if (temp[i][j-1] != null) n.add_neighbor(temp[i][j-1]);
+                }
+                if ((i - 1) >= 0) {
+                    // the position to the left
+                    if (temp[i-1][j] != null) n.add_neighbor(temp[i-1][j]);
+                }
+                if ((( j + 1) < this.numRows) && ((i - 1) >= 0)){
+                    // position to the top left
+                    if (temp[i-1][j+1] != null) n.add_neighbor(temp[i-1][j+1]);
+                }
+                if (((i - 1) >= 0) && (( j - 1) >= 0)) {
+                    // position to the top right
+                    if (temp[i-1][j-1] != null) n.add_neighbor(temp[i-1][j-1]);
+                }
+                graph.add(n);
+            }
+        }
+
+        return graph;
+    }
+
+
 
     public static void main (String[] args) {
 
         try {
+
             World myWorld = new World("maps/myMap.txt", false);
 
             System.out.println(myWorld.getStartPos());
@@ -39,13 +97,15 @@ public class myRobot extends Robot {
 
             System.out.println(myWorld.numRows());
 
-            myRobot robo = new myRobot();
+            myRobot robo = new myRobot(myWorld.numCols(), myWorld.numRows(), myWorld.getEndPos(), false);
 
             robo.addToWorld(myWorld);
 
             robo.travelToDestination();
 
             System.out.println("Went to location");
+
+            System.out.println(robo.pingMap(new Point(4,4)));
         }
 
         catch (Exception e) {
